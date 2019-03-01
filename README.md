5cda8a2e5f5e6561482c9d1f93de9a1f00e1442e
Balázs Keresztury <b@belidzs.hu>
2019-03-01 19:51:55 +0100

    Cleanup before publishing

---
+++ README.md
@@ -0,0 +1,31 @@
+# Alpha Vantage to GnuCash price downloader
+
+This script can import daily stock and forex quotes from [Alpha Vantage](https://www.alphavantage.co/) and insert them 
+into [GnuCash](https://gnucash.org/)'s database.
+
+## Features
+* Connect to any type of GnuCash data source supported by [piecash](https://github.com/sdementen/piecash) 
+(*.gnucash files, relational databases, plain XML, etc)
+* It can run even without GnuCash installed
+* Automatic throttling to avoid over-polling
+* Ignores price if it was already downloaded that day
+
+## Requirements
+* Python 3
+* [piecash](https://github.com/sdementen/piecash)
+* [alpha-vantage](https://github.com/RomelTorres/alpha_vantage)
+* python-dateutil
+
+## Install requirements
+Just run `pip install -r requirements.txt`
+
+It is recommended to install dependencies inside a dedicated virtualenv.
+
+## Usage
+Edit script to set up
+
+* Alpha Vantage API key 
+* Connection parameters
+* Namespace - currency pairs
+
+Run: `python gnucash_quotes.pty`
--- gnucash_quotes.py
+++ gnucash_quotes.py
@@ -6,18 +6,29 @@ import dateutil.parser
 from decimal import *
 from time import time, sleep
 
+# set your own http://alphavantage.co/ API key as an environment variable
 ALPHAVANTAGE_API_KEY = environ['ALPHAVANTAGE_API_KEY']
-API_MAX_CALLS_PER_INTERVAL = 5
-API_MAX_CALLS_INTERVAL = 60
+
+# assign currencies 
 NAMESPACE_CURRENCY = {
     'Amsterdam': 'EUR',
     'NASDAQ': 'USD',
     'London': 'GBP'
 }
+
+# here you can skip namespaces which are not supported by alpha vantage
 NAMESPACE_SKIP = {
     'template',
     'ÁK'
 }
+
+# piecash connection string to gnucash. this can be anything GnuCash itself supports
+# docs are here: https://piecash.readthedocs.io/en
+PIECASH_URI_CONN = 'postgresql://postgres@localhost/gnucash'
+
+# throttling settings: currently 5 calls/minute
+API_MAX_CALLS_PER_INTERVAL = 5
+API_MAX_CALLS_INTERVAL = 60
 
 forex = ForeignExchange(key=ALPHAVANTAGE_API_KEY)
 stockexchange = TimeSeries(key=ALPHAVANTAGE_API_KEY)
@@ -29,27 +40,24 @@ call_counter = 0
 def add_price(commodity, currency, date, value):
     try:
         price = book.get(Price, commodity=commodity, currency=currency, date=date)
-        print('Az árfolyam már létezik: {0}'.format(price))
+        print('Exchange rate already exists: {0}'.format(price))
     except ValueError:
         price = Price(commodity=commodity, currency=currency, date=date, value=value)
-        print('Új árfolyam: {0}'.format(price))
+        print('New exchange rate acquired: {0}'.format(price))
 
 
-with open_book(uri_conn='postgresql://postgres@localhost/gnucash', readonly=False, do_backup=False) as book:  # type: Book
+with open_book(uri_conn=PIECASH_URI_CONN, readonly=False, do_backup=False) as book:  # type: Book
     for commodity in book.commodities:  # type: Commodity
         if commodity != book.default_currency and commodity.namespace not in NAMESPACE_SKIP:
-            print('Letöltés: {0}'.format(commodity.mnemonic))
+            print('Downloading: {0}'.format(commodity.mnemonic))
             if call_counter >= API_MAX_CALLS_PER_INTERVAL and time() - start < API_MAX_CALLS_INTERVAL:
-                print('Alvás {0} másodpercig'.format(int(API_MAX_CALLS_INTERVAL - (time() - start))))
+                print('Sleeping {0} seconds'.format(int(API_MAX_CALLS_INTERVAL - (time() - start))))
                 sleep(API_MAX_CALLS_INTERVAL - (time() - start))
                 call_counter = 0
                 start = time()
             mnemonic = commodity.mnemonic
             if commodity.namespace == 'CURRENCY':
                 factor = Decimal(1.0)
-                if commodity.mnemonic == 'XXX':
-                    mnemonic = 'BTC'
-                    factor = Decimal(0.001)
                 price_av, meta = forex.get_currency_exchange_rate(mnemonic, book.default_currency.mnemonic)
                 price_date = dateutil.parser.parse(timestr=price_av['6. Last Refreshed']).date()
                 price_value = round(Decimal(price_av['5. Exchange Rate']) * factor, 8)
